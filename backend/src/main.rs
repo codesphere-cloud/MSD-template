701ab54b4eb52814846142a9001f113075ee29da
jan-david.wiederstein <jan-david.wiederstein@codesphere.com>
2024-06-25 22:07:57 +0200

    hi

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,7 +6,7 @@ use actix_web::{get, post, put, web, App, HttpServer, Responder, HttpResponse};
 use crate::models::{User, Tweet, NewTweet, Comment, NewComment, TweetWithUser, CommentWithUser};
 use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
-use crate::schema::{users::dsl as users_dsl, tweets, tweets::dsl as tweets_dsl, comments::dsl as comments_dsl};
+use crate::schema::{users::dsl as users_dsl, tweets, tweets::dsl as tweets_dsl, comments, comments::dsl as comments_dsl};
 use std::env;
 
 mod schema;
