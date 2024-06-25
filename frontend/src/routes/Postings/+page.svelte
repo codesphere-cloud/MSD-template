3674cf5cc674a97a3b4673217d7a02e7bee9be91
jan-david.wiederstein <jan-david.wiederstein@codesphere.com>
2024-06-25 20:41:40 +0200

    hi

--- frontend/src/routes/Postings/+page.svelte
+++ frontend/src/routes/Postings/+page.svelte
@@ -67,7 +67,7 @@
 		const comments = await response.json();
 		// Füge die geladenen Kommentare dem entsprechenden Tweet hinzu
 		tweets[i].comments = comments;
-		tweest[i].showComments = false;
+		tweets[i].showComments = false;
 		} else {
 		console.error('Error fetching comments:', response.statusText);
 		}
