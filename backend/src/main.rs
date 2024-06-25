70d34eea4f545098d65e613dc296726080cca1e1
jan-david.wiederstein <jan-david.wiederstein@codesphere.com>
2024-06-25 22:03:46 +0200

    hi

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,6 +4,7 @@ extern crate diesel_migrations;
 
 use actix_web::{get, post, put, web, App, HttpServer, Responder, HttpResponse};
 use crate::models::{User, Tweet, NewTweet, Comment, NewComment, TweetWithUser, CommentWithUserName};
+use crate::models::CommentWithUserName;
 use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
 use crate::schema::{users::dsl as users_dsl, tweets, tweets::dsl as tweets_dsl, comments::dsl as comments_dsl};
